9fc616c17a73d4dba86c920c15a6b2d81c52eb40
elise <Elisenovaes10@gmail.com>
2023-08-07 22:19:02 -0300

    Finalização do componente BlogC

--- NuClone/App.styles.ts
+++ NuClone/App.styles.ts
@@ -12,5 +12,26 @@ export const BlogCView = styled.View`
     margin-top: 24px;
 `
 export const BlogCItem = styled.View`
-    margin-right:8px`
+    margin-right:8px
+`
+export const CFunction = styled.View`
+border-top-width: 1px;
+margin-top: 24px;
+border-color: #e6e6e6;
+
+padding: 24px;
+`
+export const CFunctionRow = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`
+export const Touchable = styled.TouchableOpacity`
+    padding: 8px;
+    background-color: #e6e6e6;
+    width: 130px;
+    border-radius: 16px;
+    align-items: center;
+    margin-top: 16px;
+`
 ;
--- NuClone/App.tsx
+++ NuClone/App.tsx
@@ -1,17 +1,19 @@
 
 import React from 'react';
 import { Text, StyleSheet } from 'react-native'
-import { BlogCView, CFCView, Container, BlogCItem } from './App.styles';
+import { BlogCView, CFCView, Container, BlogCItem, CFunction, CFunctionRow, Touchable } from './App.styles';
 import { Header } from './components/Header/Header';
 import { CFC } from './components/CFC/CFC';
 import { BlogC } from './components/BlogC/BlogC';
 import { ScrollView } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 
 
 export default function App() {
   return (
     <Container>
+      <ScrollView contentContainerStyle={{paddingBottom: 50}}>
       <Header />
       <CFCView>
         <CFC />
@@ -44,10 +46,31 @@ export default function App() {
                   e desbloqueie brasões incríveis.</Text>} />
           </BlogCItem>
 
-
         </ScrollView>
       </BlogCView>
+      <CFunction>
+        <CFunctionRow>
+          <Text style={styles.writingTitle}>Cartão de Crédito</Text>
+          <Ionicons name='chevron-forward' size={15} color={"#c2bebe"}/>
+        </CFunctionRow>
+        <Text style={styles.textGray}>Fatura atual</Text>
+        <Text style={styles.title}>R$ 196,68</Text>
+        <Text style={styles.textGray}>Limite disponível de R$ 5.000,00</Text>
+        <Text style={styles.textGray}>Limite adicional para Pix e boletos: <Text style={styles.textPurple}> R$500,00</Text></Text>
 
+        <Touchable>
+          <Text style={styles.textButton}>Parcelar compras</Text>
+        </Touchable>
+      </CFunction>
+      <CFunction>
+      <CFunctionRow>
+          <Text style={styles.writingTitle}>Empréstimo</Text>
+          <Ionicons name='chevron-forward' size={15} color={"#c2bebe"}/>
+        </CFunctionRow>
+        <Text style={styles.textGray}>Valor disponível de até</Text>
+        <Text style={styles.title}>R$ 10.000,00</Text>
+      </CFunction>
+      </ScrollView>
     </Container>
   );
 }
@@ -58,5 +81,31 @@ const styles = StyleSheet.create({
   },
   textPurple: {
     color: '#820ad1'
+  },
+  title:{
+    fontSize: 20,
+    fontWeight:'bold',
+    marginTop: 6
+
+  },
+  writingTitle:{
+    fontSize: 20,
+    fontWeight:'bold',
+
+  },
+  subtitle:{
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  textGray: {
+    color: '#c2bebe',
+    fontWeight: 'bold',
+    marginTop: 8
+
+  },
+  textButton: {
+    fontSize: 14,
+    fontWeight: 'bold'
+
   }
 })
